8eb6c58bb53c740bfeca199ad053b5c2f10eaf01
SteveAsche <steve.asche@gmail.com>
2019-01-23 22:22:01 -0800

    Commits as of page 422 in python crash course
    This is the point where the files blew up last time

---
+++ learning_logs/urls.py
@@ -0,0 +1,14 @@
+"""Defines URL patterns for learing_logs."""
+
+from django.urls import path
+from . import views
+
+app_name = 'learning_logs'
+
+urlpatterns = [ 
+	#Home page
+	path('', views.index, name='index'),
+
+	# Show all topics
+	path('topics/', views.topics, name='topics'),
+]
